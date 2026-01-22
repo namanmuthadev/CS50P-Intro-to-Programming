f54506d4d0ba10c56909821a4d171fc81b0a8996
Naman Mutha <namanmutha.dev@gmail.com>
2026-01-22 20:24:41 +0530

    feat: implement interactive guessing game with dynamic levels

---
+++ CS50P-Intro-To-programming/libraries/game.py
@@ -0,0 +1,31 @@
+import random
+while True:
+    try:
+        level=int(input('Enter the level: '))
+        if level<=0:
+            continue
+        break
+    except ValueError:
+        print('enter a numeric value')
+        continue
+secret = random.randint(1, level)
+while True:
+    try:
+        guess = int(input(f"Guess: "))
+        if guess<1:
+            continue
+        if guess < secret:
+            print("Too small!")
+        elif guess > secret:
+            print("too large!")
+        else:
+            print("Just right! ")
+            break
+    except ValueError:
+        print("Please enter a numeric guess.")
+
+
+
+
+
+
